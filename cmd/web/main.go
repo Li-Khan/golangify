f86bd4479a14f1873ced78a27fc18fae2ce0680a
Li-Khan <alihanzh40@gmail.com>
2022-01-31 14:15:57 +0600

    получаем доступ к статическим файлам

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -11,7 +16,31 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+			return nil, err
+		}
+	}
+	return f, nil
 }
