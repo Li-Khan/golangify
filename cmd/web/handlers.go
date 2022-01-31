70dcac8701e954c7bc3cc1099da89e827fbb7915
Li-Khan <alihanzh40@gmail.com>
2022-01-31 23:03:44 +0600

    отображение контента из sqlite в html шаблон

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"text/template"
 
 	"github.com/Li-Khan/golangify/pkg/models"
 )
@@ -60,7 +61,24 @@ func (app *Application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%v", s)
+	data := &templateData{Snippet: s}
+
+	files := []string{
+		"./ui/html/show.page.html",
+		"./ui/html/base.layout.html",
+		"./ui/html/footer.partial.html",
+	}
+
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
 func (app *Application) createSnippet(w http.ResponseWriter, r *http.Request) {
